712d5c9315ea352d05b011ceda9568fb873c1444
Borin <gabriel_borin.lock@hotmail.com>
2018-07-05 17:55:37 -0300

    funfando daquele jeito

--- dropbox.c
+++ dropbox.c
@@ -1,119 +1,6 @@
-#include <stdio.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-void print_stat(struct stat buf){
-	printf ("\n\t inode: %lld \
-		 \n\t size:   %lld \
-		 \n\t block size: %lld \
-		 \n\t n blocks: %lld \
-		 \n\t acesso: %s \
-	 modif:  %s \
-	 status: %s ",
-		(long long) buf.st_ino,
-		(long long) buf.st_size,
-		(long long) buf.st_blksize,
-		(long long) buf.st_blocks,
-		ctime(&buf.st_atime),
-		ctime(&buf.st_mtime),
-		ctime(&buf.st_ctime));
-}
-
-#define print_type(t) printf(" %s ", #t);
-
-void print_type_name(unsigned char type) {
-	switch(type) {
-		case DT_DIR: print_type(DT_DIR); break;
-		case DT_REG: print_type(DT_REG); break;
-	}
-}
-
-int main(int argc, char **argv){
-  struct dirent **namelist;
-  int n, i;
-  struct stat buf;
-  while(1){
-    //Verifica se os arquivos foram modificados a cada 10 segundos
-    if (argc == 1) {
-      n = scandir(".", &namelist, NULL, alphasort);
-    } else {
-      n = scandir(argv[1], &namelist, NULL, alphasort);
-    }
-
-    if (n < 0)
-        perror("scandir()");
-    else {
-      i = 0;
-      while (i < n) {
-        printf("%s", namelist[i]->d_name);
-        print_type_name(namelist[i]->d_type);
-        stat(namelist[i]->d_name, &buf);
-        print_stat(buf);
-        free(namelist[i]);
-        i++;
-      }
-      free(namelist);
-    }
-    return 0;
-    //Se foi modificado faz backup
-
-  }
-}
-
-int main(int argc, char **argv) {
-	if(argc != 3) {
-		printf("uso: %s <arq_origem> <arq_destino>\n", argv[0]);
-		return 0;
-	}
-	char *origem = argv[1];
-	char *destino = argv[2];
-	int fd_o, fd_d;
-	fd_o = open(origem, O_RDONLY);
-	if (fd_o == -1) {
-		perror("open()");
-		return 0;
-	}
-	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-	fd_d = open(destino, O_CREAT | O_RDWR | O_APPEND, mode);
-	if(fd_d == -1) {
-		perror("open()");
-		close(fd_o);
-		return 0;
-	}
-#define BLOCO 4096
-	int nr, ns, nw, n;
-	unsigned char buffer[BLOCO];
-	void *ptr_buff;
-	do {
-		nr = read(fd_o, buffer, BLOCO);
-		if (nr == -1) {
-			perror("read()");
-			close(fd_o);
-			close(fd_d);
-			return 0;
-		}
-		else if (nr > 0) {
-			ptr_buff = buffer;
-			nw = nr;
-			ns = 0;
-			do {
-				n = write(fd_d, ptr_buff + ns, nw);
-				if (n == -1) {
-					perror("write()");
-                	        	close(fd_o);
-	        	                close(fd_d);
-		                        return 0;
-				}
-				ns += n;
-				nw -= n;
-			} while (nw > 0);
-		}
-	}while(nr > 0);
-	close(fd_o);
-	close(fd_d);
-	return 0;
-}
+//Everton da Silva Coelho  101937
+//Gabriel Borin Takahashi 101953
+//Pedro Manhez Naresi 105615
 
 #include <stdio.h>
 #include <dirent.h>
@@ -122,41 +9,119 @@ int main(int argc, char **argv) {
 #include <sys/stat.h>
 #include <unistd.h>
 #include <time.h>
+#include <string.h>
+#include <fcntl.h>
 
+#define BLOCO 4096
 
 #define print_type(t) printf(" %s ", #t);
 
-void print_type_name(unsigned char type) {
-	switch(type) {
-		case DT_DIR: print_type(DT_DIR); break;
-		case DT_REG: print_type(DT_REG); break;
-	}
-}
-
 int main(int argc, char **argv) {
-  struct dirent **namelist;
-  int n, i;
-  struct stat buf;
+	struct dirent **namelist;
+	struct dirent **namelist_backup;
+	
+	struct stat buf;
+	struct stat buf_backup;
 
-  if (argc == 1) {
-    n = scandir(".", &namelist, NULL, alphasort);
-  } else {
-    n = scandir(argv[1], &namelist, NULL, alphasort);
-  }
+	int n, b, i, j;
 
-  if (n < 0)
-      perror("scandir()");
-  else {
-    i = 0;
-    while (i < n) {
-      printf("%s", namelist[i]->d_name);
-      print_type_name(namelist[i]->d_type);
-      stat(namelist[i]->d_name, &buf);
-      print_stat(buf);
-      free(namelist[i]);
-      i++;
-    }
-    free(namelist);
-  }
+	time_t start, finish;
+
+	char dir_origem[500] =  "myfolder/";
+	char dir_dest[500] = "myfolder_backup/";
+
+	if (argc == 1) {
+		n = scandir(dir_origem, &namelist, NULL, alphasort);
+		b = scandir(dir_dest, &namelist_backup, NULL, alphasort);
+	}
+	if (n < 0 || b < 0)
+		perror("scandir()");
+	else {
+		while(1){
+		    i = 2;
+		    while (i < n) {
+		    	char nome1[355] = "myfolder/";
+		    	j = 2;
+		    		while (j < b){
+		    			char nome2[355] = "myfolder_backup/";
+				    	if (strcmp(namelist[i]->d_name, namelist_backup[j]->d_name)==0){
+				    		if(stat(strcat(nome1, namelist[i]->d_name), &buf)==-1){
+				    			printf("Erro1\n");
+				    		}
+				    		else{
+				    			printf("%s\n", ctime(&buf.st_mtime));
+				    		}
+				    		if(stat(strcat(nome2, namelist_backup[j]->d_name), &buf_backup)==-1){
+				    			printf("Erro2\n");
+				    		}
+				    		else{
+				    			printf("%s\n", ctime(&buf_backup.st_mtime));
+				    		}
+				   
+				    		start = buf.st_mtime;
+				    		finish = buf_backup.st_mtime;
+
+				    		if (difftime(start, finish) > 0){
+
+				    			printf("MODIFICOU\n");
+
+				    			char *origem = dir_origem;
+				    			printf("%s\n", origem);
+								char *destino = dir_dest;
+								printf("%s\n", destino);
+								int fd_o, fd_d;
+								fd_o = open(nome1, O_RDONLY);
+								if (fd_o == -1) {
+									perror("open(1)");
+									return 0;
+								}
+								mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+								fd_d = open(nome2, O_CREAT | O_RDWR | O_TRUNC, mode);
+								if(fd_d == -1) {
+									perror("open(2)");
+									close(fd_o);
+									return 0;
+								}
+								int nr, ns, nw, n;
+								unsigned char buffer[BLOCO];
+								void *ptr_buff;
+								do {
+									nr = read(fd_o, buffer, BLOCO);
+									if (nr == -1) {
+										perror("read()");
+										close(fd_o);
+										close(fd_d);
+										return 0;
+									}
+									else if (nr > 0) {
+										ptr_buff = buffer;
+										nw = nr;
+										ns = 0;
+										do {
+											n = write(fd_d, ptr_buff + ns, nw);
+											if (n == -1) {
+												perror("write()");
+							                	        	close(fd_o);
+								        	                close(fd_d);
+									                        return 0;
+											}
+											ns += n;
+											nw -= n;
+										} while (nw > 0);
+									}
+								}while(nr > 0);
+								close(fd_o);
+								close(fd_d);
+				    		}
+				    	}
+				    	j++;
+				    	strcpy(nome2, "");
+				    }
+				i++;
+				strcpy(nome1, "");
+		    }
+		    sleep(3);
+		}
+	}
   return 0;
 }
